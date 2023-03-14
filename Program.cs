62d33f4a7be5d5e12e2d1182196b3d7bb6fcdfed
Davi Zappala <94610458+Davizappala@users.noreply.github.com>
2023-03-14 14:59:41 -0300

    Add Files

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using triangulo;
+
+namespace AtvAed4
+{
+    internal class Triangulo
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Tipo de triangulo");
+            Tri p1 = new Tri();
+            Console.WriteLine("Digite o valor de A");
+
+            p1.SetA(float.Parse(Console.ReadLine()));
+            Console.WriteLine("Digite o valor de B");
+            p1.SetB(float.Parse(Console.ReadLine()));
+            Console.WriteLine("Digite o valor de C");
+            p1.SetC(float.Parse(Console.ReadLine()));
+
+            Console.WriteLine("O valor de A é: " + p1.GetA());
+            Console.WriteLine("O valor de B é: " + p1.GetB());
+            Console.WriteLine("O valor de C é: " + p1.GetC());
+
+
+            p1.PrintDados();
+        }
+    }
+}
---
+++ tri.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace triangulo
+{
+    internal class Tri
+    {
+
+        private float A;
+        private float B;
+        private float C;
+
+        public Tri() { }
+
+        public Tri(float A, float B, float C)
+        {
+
+            this.A = A;
+            this.B = B;
+            this.C = C;
+        }
+
+        public void SetA(float A) { this.A = A; }
+       
+        public float GetA()
+        {
+            return this.A;
+        }
+
+        public void SetB(float B) { this.B = B; }
+
+        public float GetB()
+        {
+            return this.B;
+        }
+
+
+        public void SetC(float C) { this.C = C; }
+
+        public float GetC()
+        {
+            ; return this.C;
+        }
+
+        public void PrintDados()
+        {
+            if ((A == B) && (B == C))
+            {
+                Console.WriteLine("Triangulo Equilatero");
+            }
+
+            else if (A >= B + C)
+            {
+                Console.WriteLine("Nao forma triangulo");
+            }
+
+            else if ((A * A == B * B + C * C))
+            {
+                Console.WriteLine("Triangulo Retangulo");
+            }
+
+            else if ((A * A > B * B + C * C))
+            {
+                Console.WriteLine("Triangulo Obtusangulo");
+            }
+
+            else if ((A * A < B * B + C * C))
+            {
+                Console.WriteLine("Triangulo ACUTANGULO");
+            }
+
+            else
+            {
+                Console.WriteLine("TRIANGULO ISOSCELES");
+            }
+        }
+    }
+}
